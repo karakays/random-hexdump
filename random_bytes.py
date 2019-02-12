12006e089eadca6408e8c9837612bdf4acea6e7c
Selçuk Karakayalı <skarakayali@gmail.com>
2019-02-12 22:01:10 +0000

    refactor

--- random_bytes.py
+++ random_bytes.py
@@ -3,46 +3,21 @@ import os
 import base64
 import random
 import numpy
-from flask import Flask
-from flask import Response
 
 
-app = Flask(__name__)
-
-BYTE_SIZE = 256
-
-def random():
-    while True:
-        time.sleep(1)
-        block = random_block("SELCUK", "KARAKAYALI")
-        f = ""
-        for elem in zip(block[0].split(os.linesep),
-                        block[1].split(os.linesep)):
-            f += (f"{elem[0]}{elem[1]: >20}\n")
-        yield(
-            f'''--bytes\r\n
-            Content-Disposition: x-www-form-urlencoded\r\n\r\n
-            {f}
-            \r\n''')
-
-
-@app.route("/")
-def hello_world():
-    return Response(random(), mimetype='multipart/form-data; boundary=bytes')
-
-
-# TODO make this a generator
-# TODO include offset so (offset) (16 bytes) (ascii)
-# TODO use numparray instead of tuple
-def random_block(name):
-    bytez = bytearray(os.urandom(BYTE_SIZE))
-
-    start, end = (random.randint(0, BYTE_SIZE - len(name)), len(name))
-
+def random_hex(name=None, byte_len=256):
+    bytez = bytearray(os.urandom(byte_len))
+    start = random.randint(0, byte_len - len(name))
+    end = start + len(name)
     bytez[start:end] = name.encode('ascii')
+    yield (numpy.array(bytez).reshape(byte_len // 16, 16))
 
-    np_arr = np.array(bytez).reshape(BYTE_SIZE / 16, 16)
 
+def to_ascii(bytez):
+    pass
+
+
+def hex_dump(byte_arr, offset=None):
     hex_chars = base64.b16encode(bytez).decode()
 
     hex_block = ""
@@ -52,7 +27,6 @@ def random_block(name):
             i = r + c
             hex_block += hex_chars[i:i+2] + " "
         hex_block += os.linesep
-    print(hex_block)
     ascii_block = ""
 
     ascii = bytez.decode('ascii', 'replace').replace('\ufffD', '.')
@@ -63,5 +37,7 @@ def random_block(name):
     yield (ascii_block)
 
 
-for n in random_block("SELCUK KARAKAYALI"):
-    print(n)
+if __name__ == '__main__':
+    for n in random_hex("SELCUK KARAKAYALI"):
+        time.sleep(2)
+        print(n)
