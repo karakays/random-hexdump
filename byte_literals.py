ba6596ecc6a2a6cd5d07f184cc18faac87478098
Selçuk Karakayalı <skarakayali@gmail.com>
2019-02-17 15:55:31 +0300

    practice

--- byte_literals.py
+++ byte_literals.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+# bytes built-in type is represented as ascii text in its output
 
 # byte literals prefix with b
 byte = b'\x16'
 print(f'value: {byte}')
 
-# byte literals are resolbed to bytes built-in type
-print(f'type: {type={byte}}')
+# byte literals are resolved into bytes built-in type
+print(f'type: type={byte}')
 
 # 0x61 matches ASCII 'a' character
 byte = b'\x61'
@@ -15,6 +16,12 @@ print(byte)
 # '97' is not integer 97 value, its ASCII encoded value
 byte = b'97'
 print(f'representation: {byte}, binary value: {byte.hex()}')
+
+# bytes implements sequence and each element is a byte 0<=b<=255
+b = b'abc'
+# prints int value
+print(f"b[1]={b[1]}")
+
 
 # If it does not correspond to ASCII char, then hex value represented.
 a = b'\x128'
@@ -37,3 +44,6 @@ print('%s, %s' % (b[0], b[3]))
 print(bytes.fromhex('FF01'))
 # whitespaces ignored
 print(bytes.fromhex('FF 01 AA    BB   '))
+
+# encode as hex
+b = bytes((10, 11, 12))
