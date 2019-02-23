032e72b13e64f3dee4503bf488fb5d0275cdad43
Selçuk Karakayalı <skarakayali@gmail.com>
2019-02-23 14:05:28 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = open('VERSION').read()
+
+setup(
+    name='random-hexdump',
+    version=VERSION,
+    author='Selçuk Karakayalı',
+    author_email='skarakayali@gmail.com',
+    maintainer='Selçuk Karakayalı',
+    url='http://github.com/karakays/random-hexdump/',
+    packages=find_packages(),
+    python_requires='>=3',
+    license='MIT',
+    description='todo',
+    long_description=open('README.rst').read(),
+)
