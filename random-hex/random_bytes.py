4e7311d2f6f22012dc2c8756c35a4785b1638760
Selçuk Karakayalı <skarakayali@gmail.com>
2019-02-27 19:01:29 +0300

    more modular

--- random-hex/random_bytes.py
+++ random-hex/random_bytes.py
@@ -5,7 +5,7 @@ import random
 import numpy
 
 
-def random_hex(text=None, length=256):
+def random_hex_np(text=None, length=256):
     """
     rows of 16 bytes
     """
@@ -14,6 +14,30 @@ def random_hex(text=None, length=256):
     end = start + len(text)
     bytez[start:end] = text.encode('ascii')
     yield (numpy.array(bytez).reshape(length // 16, 16))
+
+
+def random_hex(size, iterate=100):
+    while iterate:
+        yield (os.urandom(size))
+        iterate -= 1
+
+
+def dump_line(size=0x10, offset=0x00):
+    def next():
+        nonlocal offset
+        for bytez in random_hex(size):
+            hex_col = " ".join([f"{b:02X}" for b in bytez])
+            ascii_col = bytes_to_ascii(bytes(bytez))
+            yield f"{offset:08X} {hex_col} {ascii_col}"
+            offset += size
+    return next
+
+
+def dump(size=256):
+    next_line = dump_line(size//16, 0)
+    for l in next_line():
+        print(l)
+        time.sleep(0.5)
 
 
 def bytes_to_ascii(bytez, replace='.'):
@@ -45,7 +69,4 @@ def hex_dump(byte_np, offset=0):
 
 
 if __name__ == '__main__':
-    while True:
-        r = random_hex("SELCUK KARAKAYALI")
-        time.sleep(0.5)
-        print(hex_dump(next(r)), end='')
+     dump()
