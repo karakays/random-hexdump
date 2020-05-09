d20432cd535eb219f572a243f670bc712aca4f23
Selçuk Karakayalı <skarakayali@gmail.com>
2020-05-09 16:22:11 +0300

    project renamed rhd

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 pkg_vars = {}
 
-with open("random-hexdump/_version.py") as fp:
+with open("rhd/_version.py") as fp:
     exec(fp.read(), pkg_vars)
 
 setup(
-    name='random-hexdump',
+    name='rhd',
+    description='Generate random hexdumps',
     version=pkg_vars['__version__'],
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
@@ -15,7 +16,6 @@ setup(
     packages=find_packages(),
     python_requires='>=3',
     license='MIT',
-    description='Generate random hexdumps',
     long_description=open('README.rst').read(),
     classifiers=[ "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
